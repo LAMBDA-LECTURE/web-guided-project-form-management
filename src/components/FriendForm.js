b50d20a6a09684b43c684976e3f4a4f4bb13bb50
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-18 19:38:38 +0200

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -9,6 +9,7 @@ export default function FriendForm(props) {
     // a) pull the name of the input from the event object
     // b) pull the value of the input from the event object
     // c) use the `update` callback coming in through props
+    debugger
   }
 
   const onSubmit = evt => {
